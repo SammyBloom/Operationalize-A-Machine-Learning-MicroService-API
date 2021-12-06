83869215cfbba21d29a993f6d7edbbfebbdba13c
SammyBloom <nwachiemi@gmail.com>
2021-12-06 23:20:55 +0100

    completed upload_docker.sh for uploading docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sammybloom/latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag latest $dockerpath
+docker push $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
